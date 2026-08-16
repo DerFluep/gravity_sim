bbfe681b57417a0d07f8c8b09b2c0f81bfd84833
DerFluep <36232559+DerFluep@users.noreply.github.com>
2026-08-16 18:38:43 +0200

    Correct collision, right radius calulation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,66 @@
+use std::f32::consts::PI;
 use std::time::Duration;
 
 use rand::prelude::*;
 use sdl3::event::Event;
 use sdl3::keyboard::Keycode;
 use sdl3::pixels::Color;
-use sdl3::rect::{Point, Rect};
+use sdl3::render::{Canvas, FPoint};
+use sdl3::video::Window;
 
-const WIDTH: u32 = 512;
-const HEIGHT: u32 = 512;
+const WIDTH: u32 = 1024;
+const HEIGHT: u32 = 1024;
+const G: f32 = 0.01;
+
+fn draw_circle(render: &mut Canvas<Window>, pos_x: f32, pos_y: f32, radius: f32) {
+    let diameter = radius * 2.0;
+
+    let mut x = radius - 1.0;
+    let mut y = 0.0;
+    let mut tx = 1.0;
+    let mut ty = 1.0;
+    let mut error = tx - diameter;
+
+    render.set_draw_color(Color::WHITE);
+    while x >= y {
+        render
+            .draw_point(FPoint::new(pos_x + x, pos_y - y))
+            .unwrap();
+        render
+            .draw_point(FPoint::new(pos_x + x, pos_y + y))
+            .unwrap();
+        render
+            .draw_point(FPoint::new(pos_x - x, pos_y - y))
+            .unwrap();
+        render
+            .draw_point(FPoint::new(pos_x - x, pos_y + y))
+            .unwrap();
+        render
+            .draw_point(FPoint::new(pos_x + y, pos_y - x))
+            .unwrap();
+        render
+            .draw_point(FPoint::new(pos_x + y, pos_y + x))
+            .unwrap();
+        render
+            .draw_point(FPoint::new(pos_x - y, pos_y - x))
+            .unwrap();
+        render
+            .draw_point(FPoint::new(pos_x - y, pos_y + x))
+            .unwrap();
+
+        if error <= 0.0 {
+            y += 1.0;
+            error += ty;
+            ty += 2.0;
+        }
+
+        if error > 0.0 {
+            x -= 1.0;
+            tx += 2.0;
+            error += tx - diameter;
+        }
+    }
+}
 
 struct Particles {
     x: Vec<f32>,
@@ -24,18 +77,20 @@ impl Particles {
         let mut temp_y = Vec::new();
         let mut temp_vel_x: Vec<f32> = Vec::new();
         let mut temp_vel_y: Vec<f32> = Vec::new();
+        let mut temp_m: Vec<f32> = Vec::new();
         for _ in 0..num {
-            temp_x.push(rng.random_range(0.0..WIDTH as f32));
-            temp_y.push(rng.random_range(0.0..HEIGHT as f32));
+            temp_x.push(rng.random_range(256.0..768.0));
+            temp_y.push(rng.random_range(256.0..768.0));
             temp_vel_x.push(rng.random_range(-0.5..0.5));
             temp_vel_y.push(rng.random_range(-0.5..0.5));
+            temp_m.push(rng.random_range(0.0..5.0));
         }
         Particles {
             x: temp_x,
             y: temp_y,
             vel_x: temp_vel_x,
             vel_y: temp_vel_y,
-            m: vec![0.05; num],
+            m: temp_m,
         }
     }
 
@@ -48,11 +103,14 @@ impl Particles {
                 let dir_x = self.x[i] - self.x[j];
                 let dir_y = self.y[i] - self.y[j];
                 let distance = (dir_x.powi(2) + dir_y.powi(2)).sqrt();
-                if distance < 5.0 {
+                let radius = ((self.m[i] * 3.0) / (4.0 * PI)).cbrt();
+                if distance < radius {
                     let mid_x = (self.x[i] + self.x[j]) / 2.0;
                     let mid_y = (self.y[i] + self.y[j]) / 2.0;
-                    let mid_vel_x = self.vel_x[i] + self.vel_x[j];
-                    let mid_vel_y = self.vel_y[i] + self.vel_y[j];
+                    let mid_vel_x = (self.m[i] * self.vel_x[i] + self.m[j] * self.vel_x[j])
+                        / (self.m[i] + self.m[j]);
+                    let mid_vel_y = (self.m[i] * self.vel_y[i] + self.m[j] * self.vel_y[j])
+                        / (self.m[i] + self.m[j]);
                     let new_mass = self.m[i] + self.m[j];
 
                     self.x.remove(j);
@@ -94,7 +152,7 @@ impl Particles {
                 let distance = (dir_x.powi(2) + dir_y.powi(2)).sqrt();
                 dir_x /= distance;
                 dir_y /= distance;
-                let force = (self.m[current] * self.m[n]) / distance.powi(2);
+                let force = G * (self.m[current] * self.m[n]) / distance.powi(2);
                 let acceleration = force / self.m[current];
                 vel_x += dir_x * acceleration;
                 vel_y += dir_y * acceleration;
@@ -141,12 +199,13 @@ fn main() {
 
         canvas.set_draw_color(Color::WHITE);
         for particle in 0..particles.x.len() {
-            canvas
-                .draw_point(Point::new(
-                    particles.x[particle] as i32,
-                    particles.y[particle] as i32,
-                ))
-                .unwrap();
+            let radius = ((particles.m[particle] * 3.0) / (4.0 * PI)).cbrt();
+            draw_circle(
+                &mut canvas,
+                particles.x[particle],
+                particles.y[particle],
+                radius + 1.0,
+            );
         }
 
         canvas.present();
